35d9896add35ff7456fdcc2517bbd70ae52f23e7
xjakubeco <102169575+xjakubeco@users.noreply.github.com>
2022-03-24 21:18:21 +0100

    Added some loops

--- scripts.js
+++ scripts.js
@@ -1,2 +1,39 @@
 console.log(2+2);
 console.log("Oliver Jakubec");
+
+
+let numbers = 11;
+for (let i = 1; i < numbers; i++) {
+    console.log(i);
+    
+}
+
+for (let a = 1; a < 21; a++) {
+    if (a % 2 ==0) {
+        console.log(a);
+    }
+}
+
+
+
+let num = 20;
+for (let a = 1; a <= num; a++) {
+    
+        console.log(a);
+    
+}
+
+
+
+let array = [0,3,6,7,9];
+
+
+for (let i = 0; i < array.length; i++) {
+    
+    console.log(array[i]);
+}
+
+
+
+array.forEach(element => console.log(element));
+
